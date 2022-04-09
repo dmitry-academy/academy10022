08c79025ae94bac65089d5576ae2df8d47ee1066
Dzmitry Shchepin <Dzmitry.Shchepin@gmail.com>
2022-04-09 12:55:57 +0300

    lesson10

--- academy10022/src/main/java/by/academy/lesson7/ModificatorsDemo2.java
+++ academy10022/src/main/java/by/academy/lesson7/ModificatorsDemo2.java
@@ -6,8 +6,8 @@ public class ModificatorsDemo2 {
 	public static void main(String[] args) {
 		Modificators object = new Modificators();
 
-		object.defaultVar = 10;// Ошибка компиляции!
+//		object.defaultVar = 10;// Ошибка компиляции!
 		object.publicVar = 20;
-		object.privateVar = 100; // Ошибка компиляции!
+//		object.privateVar = 100; // Ошибка компиляции!
 	}
 }
